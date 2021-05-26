18f38ca640db0424aad2b289f9a1f7acfb258037
Joseph <jwhenry28@gmail.com>
2021-05-26 09:57:49 -0500

    Moving to CSIL

--- scratch.py
+++ scratch.py
@@ -1,6 +1,7 @@
 import pickle
 import json
 import pprint
+import random
 
 # with open("results/clean", "r") as f:
 #     data = json.load(f)
@@ -11,8 +12,24 @@ import pprint
 with open("results/raw_data", "rb") as infile:
     data = pickle.load(infile)
 
-with open("results/clean", "w") as outfile:
+with open("results/notated_clean", "w") as outfile:
     json.dump(data, outfile)
+
+
+# def chunkIt(seq, num):
+#     avg = len(seq) / float(num)
+#     out = []
+#     last = 0.0
+#     while last < len(seq):
+#         out.append(seq[int(last):int(last + avg)])
+#         last += avg
+#     return out
+
+# conc = [random.randint(0, 100) for i in range(1000)]
+# altconc = [sum(x) for x in chunkIt(conc, 70)]
+
+# print("len conc:", len(conc))
+# print("len altconc:", len(altconc))
 
 """
 NOTES
