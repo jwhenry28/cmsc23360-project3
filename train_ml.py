228548a06d6ddabdb17fa25d0522836129a8cef2
jwhenry28 <jwhenry28@linux2.cs.uchicago.edu>
2021-05-28 15:50:48 -0500

    Final script

--- train_ml.py
+++ train_ml.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import random
 import time
 import sys
 from sklearn.model_selection import train_test_split
@@ -20,59 +22,47 @@ for site in bad_sites:
 y = fingerprint_data.pop('siteClass')
 fingerprint_data.pop('siteInstance')
 
-fs = SelectKBest(score_func=f_classif, k=5)
-X_new = fs.fit_transform(fingerprint_data, y)
-X_filter = fs.get_support()
-
-print(fingerprint_data.columns[X_filter])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-sys.exit(0)
-
-
 # Initialize accuracy dictionary
 accuracies = {}
 for col in fingerprint_data.columns:
-    accuracies[col] = -1
+    accuracies[col] = []
 
 # Loop through each feature
+seeds = [random.randint(0, 1000) for i in range(0, 30)]
 print("[*] Considering", len(fingerprint_data.columns), "columns.")
+count = 1
 for feature in fingerprint_data.columns:
     tmp_df = fingerprint_data.loc[:, fingerprint_data.columns != feature]
 
-    # Used: https://www.geeksforgeeks.org/random-forest-classifier-using-scikit-learn/
-    # Split data into testing and training
-    X_train, X_test, y_train, y_test = train_test_split(tmp_df, y, test_size=0.10, stratify=y, random_state=222)
+    for i in range(30):
+        # Used: https://www.geeksforgeeks.org/random-forest-classifier-using-scikit-learn/
+        # Split data into testing and training
+        X_train, X_test, y_train, y_test = train_test_split(tmp_df, y, test_size=0.10, stratify=y, random_state=seeds[i])
 
-    # Create classifier and train
-    # n_estimators chosen from this article: https://medium.com/all-things-ai/in-depth-parameter-tuning-for-random-forest-d67bb7e920d
-    clf = RandomForestClassifier(n_estimators=30)
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
+        # Create classifier and train
+        # n_estimators chosen from this article: https://medium.com/all-things-ai/in-depth-parameter-tuning-for-random-forest-d67bb7e920d
+        clf = RandomForestClassifier(n_estimators=30)
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
 
-    # Score accuracy
-    accuracy = metrics.accuracy_score(y_test, y_pred)
-    print("[*] --", feature, "--")
-    print("[*] ACCURACY:", accuracy)
-    # print("[*] Took", time.time() - start_time, "seconds.")
+        # Score accuracy
+        accuracy = metrics.accuracy_score(y_test, y_pred)
+        print("[*] --", feature, "[" + str(i) + "]", "--")
+        print("[*] ACCURACY:", accuracy)
+        # print("[*] Took", time.time() - start_time, "seconds.")
+        print("")
+
+        # Store results
+        accuracies[feature].append(accuracy)
+    print("---------------------------------------------------")
+    print(str(count) + "/" + str(len(fingerprint_data.columns)), 'features completed.')
+    print("---------------------------------------------------")
     print("")
+    count += 1
 
-    # Store results
-    accuracies[feature] = accuracy
+# Turn list into average accuracy
+for key in accuracies.keys():
+    accuracies[key] = [np.median(accuracies[key])]
 
-print("")
-
-pprint(accuracies)
+accuracies_df = pd.DataFrame(accuracies)
+accuracies_df.to_csv('ml_ranks.csv', index=False)
